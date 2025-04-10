06a5d4e5d5664cab3455bd18d332de2ddf718bde
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2025-04-10 15:42:53 +0100

    Make failing to fetch broadcasts a warning

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ fn main() -> Result<()> {
         let Ok(current_games) = current_games_result else {
             let e = current_games_result.unwrap_err();
 
-            log.error(&format!("Unable to send notify: {:?}", e));
+            log.warn(&format!("Unable to fetch in-progress games: {:?}", e));
 
             std::thread::sleep(POLL_DELAY);
             continue;
